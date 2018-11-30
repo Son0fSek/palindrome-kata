13a9297e572fd97f97c7ad0262ceff4851ded801
Son0fSek <joehayward@live.com>
2018-11-30 15:49:49 +0000

    fix repo

--- src/main/java/PalindromeChecker.java
+++ src/main/java/PalindromeChecker.java
@@ -3,7 +3,7 @@ public class PalindromeChecker {
 
     public boolean palindromeTest(String input) {
 
-        String inputForward = input.trim().replaceAll("[\\p{Punct} \\s]", "");
+        String inputForward = input.replaceAll("[\\p{Punct} \\s]", "");
 
         StringBuilder inputAsStringBuilder = new StringBuilder(inputForward);
         String inputReversed = inputAsStringBuilder.reverse().toString();
--- src/test/java/PalindromeCheckerTest.java
+++ src/test/java/PalindromeCheckerTest.java
@@ -1,6 +1,7 @@
 import org.junit.Before;
 import org.junit.Test;
-import static org.assertj.core.api.Assertions.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class PalindromeCheckerTest {
 
@@ -12,64 +13,89 @@ public class PalindromeCheckerTest {
     }
 
     @Test
-    public void givenEmptyString_ThenTrueIsReturned(){
+    public void givenEmptyString_ThenTrueIsReturned() {
         boolean result = palindromeChecker.palindromeTest("");
         assertThat(result).isEqualTo(true);
     }
 
     @Test
-    public void givenStringOfSpaces_ThenTrueIsReturned(){
+    public void givenStringOfSpaces_ThenTrueIsReturned() {
         boolean result = palindromeChecker.palindromeTest("     ");
         assertThat(result).isEqualTo(true);
     }
 
     @Test
-    public void givenA_ThenTrueIsReturned(){
+    public void givenA_ThenTrueIsReturned() {
         boolean result = palindromeChecker.palindromeTest("A");
         assertThat(result).isEqualTo(true);
     }
 
     @Test
-    public void givenAbcdefg_ThenFalseIsReturned(){
+    public void givenAbcdefg_ThenFalseIsReturned() {
         boolean result = palindromeChecker.palindromeTest("abcdefg");
         assertThat(result).isEqualTo(false);
     }
 
     @Test
-    public void givenAa_ThenTrueIsReturned(){
+    public void givenAa_ThenTrueIsReturned() {
         boolean result = palindromeChecker.palindromeTest("aa");
         assertThat(result).isEqualTo(true);
     }
 
     @Test
-    public void givenAaa_ThenTrueIsReturned(){
+    public void givenAaa_ThenTrueIsReturned() {
         boolean result = palindromeChecker.palindromeTest("aaa");
         assertThat(result).isEqualTo(true);
     }
 
     @Test
-    public void givenAbba_ThenTrueIsReturned(){
+    public void givenAbba_ThenTrueIsReturned() {
         boolean result = palindromeChecker.palindromeTest("abba");
         assertThat(result).isEqualTo(true);
     }
 
     @Test
-    public void givenRepaper_ThenTrueIsReturned(){
+    public void givenRepaper_ThenTrueIsReturned() {
         boolean result = palindromeChecker.palindromeTest("Repaper");
         assertThat(result).isEqualTo(true);
     }
 
     @Test
-    public void givenAStringWIthSpaces_ThenWillReturnCorrectResult(){
+    public void givenAStringWIthSpaces_ThenWillReturnCorrectResult() {
         boolean result = palindromeChecker.palindromeTest("Race car");
         assertThat(result).isEqualTo(true);
     }
 
     @Test
-    public void givenAPhrase_ThenWillReturnCorrectResult(){
+    public void givenAPhrase_ThenWillReturnCorrectResult() {
         boolean result = palindromeChecker
                 .palindromeTest("Are we not drawn onward, " +
                         "we few, drawn onward to new era?");
         assertThat(result).isEqualTo(true);
     }
+
+    @Test
+    public void givenSingleNumber_ThenWillReturnTrue() {
+        boolean result = palindromeChecker.palindromeTest("1");
+        assertThat(result).isEqualTo(true);
+    }
+
+    @Test
+    public void given123_ThenWillReturnFalse() {
+        boolean result = palindromeChecker.palindromeTest("123");
+        assertThat(result).isEqualTo(false);
+    }
+
+    @Test
+    public void given12321_ThenWillReturnTrue(){
+        boolean result = palindromeChecker.palindromeTest("12321");
+        assertThat(result).isEqualTo(true);
+    }
+
+    @Test
+    public void givenDecimalNumber_ThenReturnsCorrectResult(){
+        boolean result = palindromeChecker.palindromeTest("123.21");
+        assertThat(result).isEqualTo(true);
+    }
 }
+
